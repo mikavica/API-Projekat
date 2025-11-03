12e969c83cc7df636e4beddb98d3e315033ee9e8
Marko Mikavica <mikavi@Markos-MacBook-Pro.local>
2025-11-04 00:11:54 +0100

    Uradili Bazu

--- main.js
+++ main.js
@@ -1,71 +1,79 @@
-const express = require("express")
-let todos = []
+const express = require("express");
+const mongo = require("mongoose");
 
-const app = express()
-app.use(express.json())
-app.get("/todos",(req,res)=>{
-    console.log("radi")
-    return res.json(todos)
-})
-app.delete("/todos/:id",(req,res)=>{
-    const id = parseInt(req.params.id, 10)
+const MONGO_URI = "mongodb://test:test@localhost:27017/";
 
-    const exists = todos.find((item)=>{
-        if(item.id===id){
-            return true
-        }
-        return false
-            
-    })
-    if(!exists){
-        return res.json("not found")
+mongo
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("povezao sam se");
+  })
+  .catch(() => {
+    console.log("nisam uspeo");
+  });
+const todoSchema = new mongo.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+const Todo = mongo.model("todo", todoSchema);
+
+let todos = [];
+
+const app = express();
+app.use(express.json());
+app.get("/todos", async (req, res) => {
+  console.log("radi");
+  const data = await Todo.find();
+  return res.json(data);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  await Todo.findByIdAndDelete(id);
+
+  return res.json("sisaj kurac");
+});
+
+app.post("/todos", async (req, res) => {
+  try {
+    const newtodo = await Todo.create({
+      title: req.body.title,
+    });
+
+    return res.json(1);
+  } catch (error) {
+    return res.json(2);
+  }
+});
+app.patch("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todo = todos.find((item) => {
+    return item.id === id;
+  });
+
+  if (!todo) {
+    return res.json("not found");
+  }
+
+  todo = {
+    ...todo,
+    isCompleted: true,
+  };
+
+  todos = todos.map((item) => {
+    if (item.id === todo.id) {
+      return todo;
     }
-    todos = todos.filter((item)=>{
-        if(item.id===id){
-            return false
-        }
-        return true
-    })
-    return res.json("sisaj kurac")
-})
-
-app.post("/todos",(req,res)=>{
-    console.log(req.body)
-    const newtodo = {
-        title: req.body.title ,
-        isCompleted: false,
-        id: 1,
-        
-    }
-    
-
-    todos.push(newtodo)
-
-    return res.json(1)
-})
-app.patch("/todos/:id",(req,res)=>{
-    const id=parseInt(req.params.id,10)
-    let todo=todos.find((item)=>{
-        return item.id === id
-    })
-
-    if(!todo){
-        return res.json("not found")
-    }
-   
-    todo={
-        ...todo,
-        isCompleted: true,
-    }
-
-    todos=todos.map((item)=>{
-        if(item.id===todo.id){
-            return todo
-        }
-        return item
-    })
-    return res.json("cao")
-} )
-app.listen(5000,()=>{
-    console.log("listening")
-})
+    return item;
+  });
+  return res.json("cao");
+});
+app.listen(5000, () => {
+  console.log("listening");
+});
---
+++ osnove.js
@@ -0,0 +1,25 @@
+async function sacuvajUBazu() {
+    console.log("sacuvao u bazy")
+}
+
+async function saberi(broj1,broj2){
+    console.log(1)
+    await sacuvajUBazu()
+    console.log(2)
+    console.log(3)
+}
+
+// 1
+// sacuvao u bazu
+// 2
+// 3
+
+
+
+let ime="luka"
+if(ime==="luka"){
+
+}
+ime="marko"
+
+
